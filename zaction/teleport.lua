7cd4ec254b98e15b36cf7221fb959aa60e1a3a20
Sidoupiar <sidoupiar@qq.com>
2020-11-16 16:25:19 +0800

    提高兼容性

--- zaction/teleport.lua
+++ zaction/teleport.lua
@@ -53,24 +53,26 @@ function SIEXHSTeleport.Teleport( event )
 			for index = currentTick , maxSize , SIEXHSTeleport.delay do
 				if index > maxSize then break end
 				local entity = SIEXHSTeleport.GetEntity( list[index] )
-				local inventory = entity.get_inventory( defines.inventory.chest )
-				if not inventory.is_empty() then
-					local settings = SIEXHSTeleport.GetSettings( entity )
-					if settings.speed > 0 then
-						for i , itemData in pairs( settings.items ) do
-							local name = itemData[1]
-							local count = inventory.get_item_count( name )
-							if count > itemData[2] then
-								local removeCount = inventory.remove{ name = name , count = math.min( count-itemData[2] , settings.speed ) }
-								local b = true
-								for n , storageItemData in pairs( storage ) do
-									if storageItemData[1] == name then
-										storageItemData[2] = storageItemData[2] + removeCount
-										b = false
-										break
+				if entity.active and not entity.to_be_deconstructed( forceIndex ) then
+					local inventory = entity.get_inventory( defines.inventory.chest )
+					if not inventory.is_empty() then
+						local settings = SIEXHSTeleport.GetSettings( entity )
+						if settings.speed > 0 then
+							for i , itemData in pairs( settings.items ) do
+								local name = itemData[1]
+								local count = inventory.get_item_count( name )
+								if count > itemData[2] then
+									local removeCount = inventory.remove{ name = name , count = math.min( count-itemData[2] , settings.speed ) }
+									local b = true
+									for n , storageItemData in pairs( storage ) do
+										if storageItemData[1] == name then
+											storageItemData[2] = storageItemData[2] + removeCount
+											b = false
+											break
+										end
 									end
+									if b then table.insert( storage , { name , removeCount } ) end
 								end
-								if b then table.insert( storage , { name , removeCount } ) end
 							end
 						end
 					end
@@ -83,26 +85,28 @@ function SIEXHSTeleport.Teleport( event )
 			for index = currentTick , maxSize , SIEXHSTeleport.delay do
 				if index > maxSize then break end
 				local entity = SIEXHSTeleport.GetEntity( list[index] )
-				local settings = SIEXHSTeleport.GetSettings( entity )
-				if settings.speed > 0 then
-					local inventory = entity.get_inventory( defines.inventory.chest )
-					for i , itemData in pairs( settings.items ) do
-						local name = itemData[1]
-						local count = inventory.get_item_count( name )
-						if count < itemData[2] then
-							local storageItemIndex = 0
-							local storageItemCount = 0
-							for n , storageItemData in pairs( storage ) do
-								if storageItemData[1] == name then
-									storageItemIndex = n
-									storageItemCount = storageItemData[2]
-									break
+				if entity.active and not entity.to_be_deconstructed( forceIndex ) then
+					local settings = SIEXHSTeleport.GetSettings( entity )
+					if settings.speed > 0 then
+						local inventory = entity.get_inventory( defines.inventory.chest )
+						for i , itemData in pairs( settings.items ) do
+							local name = itemData[1]
+							local count = inventory.get_item_count( name )
+							if count < itemData[2] then
+								local storageItemIndex = 0
+								local storageItemCount = 0
+								for n , storageItemData in pairs( storage ) do
+									if storageItemData[1] == name then
+										storageItemIndex = n
+										storageItemCount = storageItemData[2]
+										break
+									end
 								end
-							end
-							if storageItemCount > 0 then
-								local insertCount = inventory.insert{ name = name , count = math.min( math.min( itemData[2]-count , storageItemCount ) , settings.speed ) }
-								storageItemCount = storageItemCount - insertCount
-								storage[storageItemIndex][2] = storageItemCount
+								if storageItemCount > 0 then
+									local insertCount = inventory.insert{ name = name , count = math.min( math.min( itemData[2]-count , storageItemCount ) , settings.speed ) }
+									storageItemCount = storageItemCount - insertCount
+									storage[storageItemIndex][2] = storageItemCount
+								end
 							end
 						end
 					end
