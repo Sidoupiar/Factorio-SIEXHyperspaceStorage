d5e025f3bcdacd5173226d10c9ba8b2638a1b93a
Sidoupiar <sidoupiar@qq.com>
2020-11-23 12:15:39 +0800

    添加三号凝胶

--- zprototype/teleporters.lua
+++ zprototype/teleporters.lua
@@ -7,28 +7,28 @@ local sizeCodes = { small = "-small" , normal = "" }
 local list =
 {
 	{
-		small = { 1 , 14 , 2 , 60 , { { SIEXHS.wallItem , 200 } } } ,
-		normal = { 3 , 44 , 2 , 120 , { { "steel-plate" , 100 } } } ,
+		small = { 1 , 19 , 2 , 60 , { { SIEXHS.wallItem , 200 } } } ,
+		normal = { 3 , 59 , 3 , 120 , { { "steel-plate" , 100 } } } ,
 		structure = { { "splitter" , 8 } , { "fast-inserter" , 5 } , { "electric-mining-drill" , 3 } }
 	} ,
 	{
-		small = { 1 , 29 , 3 , 120 , { { SIEXHS.wallItem , 300 } } } ,
-		normal = { 3 , 89 , 3 , 240 , { { "logistic-science-pack" , 250 } } } ,
+		small = { 1 , 39 , 3 , 120 , { { SIEXHS.wallItem , 300 } } } ,
+		normal = { 3 , 119 , 6 , 240 , { { "logistic-science-pack" , 250 } } } ,
 		structure = { { "laser-turret" , 7 } , { "assembling-machine-2" , 3 } , { "locomotive" , 3 } }
 	} ,
 	{
-		small = { 1 , 59 , 4 , 240 , { { SIEXHS.wallItem , 500 } } } ,
-		normal = { 3 , 179 , 5 , 480 , { { "iron-chest" , 350 } } } ,
+		small = { 1 , 79 , 4 , 240 , { { SIEXHS.wallItem , 500 } } } ,
+		normal = { 3 , 239 , 10 , 480 , { { "iron-chest" , 350 } } } ,
 		structure = { { "production-science-pack" , 5 } , { "power-switch" , 3 } , { "low-density-structure" , 3 } , { "substation" , 2 } }
 	} ,
 	{
-		small = { 1 , 119 , 6 , 480{ { SIEXHS.wallItem , 800 } } } ,
-		normal = { 3 , 359 , 9 , 960 , { { "steel-chest" , 350 } } } ,
+		small = { 1 , 159 , 6 , 480{ { SIEXHS.wallItem , 800 } } } ,
+		normal = { 3 , 479 , 14 , 960 , { { "steel-chest" , 350 } } } ,
 		structure = { { "express-underground-belt" , 5 } , { "fusion-reactor-equipment" , 3 } , { "effectivity-module-3" , 3 } , { "uranium-fuel-cell" , 5 } }
 	} ,
 	{
-		small = { 1 , 239 , 8 , 960{ { SIEXHS.wallItem , 1200 } } } ,
-		normal = { 3 , 719 , 13 , 1920 , { { "logistic-chest-buffer" , 350 } } } ,
+		small = { 1 , 319 , 8 , 960{ { SIEXHS.wallItem , 1200 } } } ,
+		normal = { 3 , 959 , 20 , 1920 , { { "logistic-chest-buffer" , 350 } } } ,
 		structure = { { "utility-science-pack" , 5 } , { "rocket-control-unit" , 10 } , { "satellite" , 1 } , { "rocket-silo" , 1 } }
 	}
 }
@@ -73,7 +73,7 @@ for level = 1 , SIEXHS.maxLevel , 1 do
 			.NewContainerLogic( "teleporter-"..typeCodeString..sizeCodeString.."-mk"..level , nil , SITypes.logisticMode.buffer )
 			.SetLocalisedNames( localisedNames )
 			.SetLocalisedDescriptions{ "SIEXHS.desc-teleporter" , { "SIEXHS.desc-"..typeCodeString } , { "SIEXHS.desc"..sizeCodeString } , { "SIEXHS.desc-use" } }
-			.SetProperties( entityData[1] , entityData[1] , entityData[1]*100 , 0 , nil , nil , entityData[2] , entityData[3] )
+			.SetProperties( entityData[1] , entityData[1] , entityData[1]*300 , 0 , nil , nil , entityData[2] , entityData[3] )
 			.SetRender_notInNetworkIcon( false )
 			.GetCurrentEntityItemName()
 			
@@ -86,7 +86,7 @@ for level = 1 , SIEXHS.maxLevel , 1 do
 			.AddCosts( structureItem , 40 )
 			.AddCosts( SIEXHS.powerItems[level] , level*200 )
 			.AddCosts( SIEXHS.coreItem , level )
-			.AddCosts( SIEXHS.gelItem , level*150 )
+			.AddCosts( SIEXHS.gel7Item , level*150 )
 			.AddCosts( lastItemName[typeCode] )
 			.AddResults( itemName )
 			.AddLastLevel( 2 )
