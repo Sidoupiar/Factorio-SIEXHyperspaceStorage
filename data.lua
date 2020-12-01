c00ef618848f0c5d2bcbdf4378b5841b28ff7caa
Sidoupiar <sidoupiar@qq.com>
2020-12-01 22:16:16 +0800

    添加成就

--- data.lua
+++ data.lua
@@ -19,3 +19,27 @@ SIGen.Finish()
 for name , recipe in pairs( SIGen.GetList( SITypes.recipe ) ) do
 	if name:sub( 1 , 6 ) == "siexhs" or recipe.category == SIEXHS.recipeType then table.insert( SIEXHS.moduleLimit , name ) end
 end
+
+-- ------------------------------------------------------------------------------------------------
+-- ---------- 添加成就 ----------------------------------------------------------------------------
+-- ------------------------------------------------------------------------------------------------
+
+data:extend
+{
+	{
+		type = "build-entity-achievement" ,
+		name = "siexhs-achievement-got-grass" ,
+		icon = "__SIEXHyperspaceStorage__/zpic/achievement/got-grass.png" ,
+		icon_size = 128 ,
+		to_build = "siexhs-container-clover-machine" ,
+		order = "si-1"
+	} ,
+	{
+		type = "build-entity-achievement" ,
+		name = "siexhs-achievement-its-over" ,
+		icon = "__SIEXHyperspaceStorage__/zpic/achievement/its-over.png" ,
+		icon_size = 128 ,
+		to_build = "siexhs-container-teleporter-input-mk5" ,
+		order = "si-2"
+	}
+}
