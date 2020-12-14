57abdc7984601f82378248ee4a13e32078cc2c60
Sidoupiar <sidoupiar@qq.com>
2020-12-14 23:24:49 +0800

    优化代码

--- zprototype/modules.lua
+++ zprototype/modules.lua
@@ -2,27 +2,26 @@
 -- ---------- 创建材料 ----------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------------------------
 
-local effects =
-{
-	speed = function( level )
-		return
-		{
-			[SITypes.moduleEffect.speed] = { bonus = level*1.2 } ,
-			[SITypes.moduleEffect.product] = { bonus = -1.5 } ,
-			[SITypes.moduleEffect.consumption] = { bonus = level*2 } ,
-			[SITypes.moduleEffect.pollut] = { bonus = level*1.2 }
-		}
-	end ,
-	product = function( level )
-		return
-		{
-			[SITypes.moduleEffect.speed] = { bonus = -6 } ,
-			[SITypes.moduleEffect.product] = { bonus = level*0.3 } ,
-			[SITypes.moduleEffect.consumption] = { bonus = level*3.5 } ,
-			[SITypes.moduleEffect.pollut] = { bonus = level*2 }
-		}
-	end
-}
+function Speed( level )
+	return
+	{
+		[SITypes.moduleEffect.speed] = { bonus = level*1.2 } ,
+		[SITypes.moduleEffect.product] = { bonus = -0.75 } ,
+		[SITypes.moduleEffect.consumption] = { bonus = level*2 } ,
+		[SITypes.moduleEffect.pollut] = { bonus = level*1.2 }
+	}
+end
+
+function Product( level )
+	return
+	{
+		[SITypes.moduleEffect.speed] = { bonus = -6 } ,
+		[SITypes.moduleEffect.product] = { bonus = level*0.15 } ,
+		[SITypes.moduleEffect.consumption] = { bonus = level*3.2 } ,
+		[SITypes.moduleEffect.pollut] = { bonus = level*1.7 }
+	}
+end
+
 SIGen.NewSubGroup( "hyperspace-module" )
 .NewTypeModule( "siexhs-module-speed" )
 .NewTypeModule( "siexhs-module-product" )
@@ -32,26 +31,66 @@ SIGen.NewSubGroup( "hyperspace-module" )
 -- ------------------------------------------------------------------------------------------------
 
 for level = 1 , SIEXHS.maxLevel , 1 do
-	for i , moduleType in pairs{ "speed" , "product" } do
-		local moduleName = SIGen.NewModule( "module-"..moduleType.."-mk"..level , 100 )
-		.SetLevel( level )
-		.SetLimitation( SIEXHS.moduleLimit , "module-limitation" )
-		.SetCustomData
-		{
-			category = "siexhs-module-" .. moduleType ,
-			effect = effects[moduleType]( level )
-		}
-		.GetCurrentEntityName()
-		
-		SIGen.NewRecipe( moduleName )
-		.SetEnergy( math.pow( level , 2 )*40 )
-		.SetEnabled( true )
-		.SetRecipeTypes( SIEXHS.recipeType )
-		.AddCosts( "electronic-circuit" , level*10+5 )
-		.AddCosts( "iron-gear-wheel" , level*15+20 )
-		.AddCosts( SIEXHS.gel7Item , level*4+8 )
-		.AddCosts( SIEXHS.autoItem )
-		.AddResults( SIPackers.SingleItemProduct( moduleName , 1 , nil , nil , 1 ) )
-		.AddLastLevel( 5 )
+	local moduleSpeedName = SIGen.NewModule( "module-speed-mk"..level , 100 )
+	.SetLevel( level )
+	.SetLimitation( SIEXHS.moduleLimit , "module-limitation" )
+	.SetCustomData
+	{
+		category = "siexhs-module-speed" ,
+		effect = Speed( level )
+	}
+	.GetCurrentEntityName()
+	
+	SIGen.NewRecipe( moduleSpeedName )
+	.SetEnergy( math.pow( level , 2 )*40 )
+	.SetEnabled( true )
+	.SetRecipeTypes( SIEXHS.recipeType )
+	.AddCosts( "electronic-circuit" , level*10+5 )
+	.AddCosts( "iron-gear-wheel" , level*15+20 )
+	.AddCosts( SIEXHS.gel7Item , level*4+8 )
+	.AddCosts( SIEXHS.autoItem )
+	.AddResults( SIPackers.SingleItemProduct( moduleSpeedName , 1 , nil , nil , 1 ) )
+	.AddLastLevel( 5 )
+	
+	local moduleProductName = SIGen.NewModule( "module-product-mk"..level , 100 )
+	.SetLevel( level )
+	.SetLimitation( SIEXHS.moduleLimit , "module-limitation" )
+	.SetCustomData
+	{
+		category = "siexhs-module-product" ,
+		effect = Speed( level )
+	}
+	.GetCurrentEntityName()
+	
+	local ingredient = nil
+	local count = 1
+	if level == 1 then
+		ingredient = moduleProductName
+		count = 2
 	end
+	
+	SIGen.NewRecipe( moduleProductName )
+	.SetEnergy( math.pow( level , 2 )*40 )
+	.SetEnabled( true )
+	.SetRecipeTypes( SIEXHS.recipeType )
+	.AddCosts( "electronic-circuit" , level*10+5 )
+	.AddCosts( "iron-gear-wheel" , level*15+20 )
+	.AddCosts( SIEXHS.gel7Item , level*4+8 )
+	.AddCosts( ingredient )
+	.AddCosts( SIEXHS.autoItem )
+	.AddResults( SIPackers.SingleItemProduct( moduleProductName , count , nil , nil , count ) )
+	.AddLastLevel( 5 )
 end
+
+SIGen.NewSubGroup( "hyperspace-others" )
+.NewRecipe( "teleporter-to-module-product-mk1" )
+.SetEnergy( 800 )
+.SetEnabled( true )
+.SetRecipeTypes( SIEXHS.recipeType )
+.AddCosts( "siexhs-item-teleporter-output-mk3" )
+.AddCosts( SIEXHS.gel7Item , 80 )
+.AddCosts( SIEXHS.autoItem , 5 )
+.AddResults( SIPackers.SingleItemProduct( "siexhs-module-module-product-mk1" , 1 , nil , nil , 1 ) )
+.AddResults( SIPackers.SingleItemProduct( SIEXHS.autoItem , 3 , nil , nil , 3 ) )
+.AddLastLevel( 5 )
+.SetSelfIcon( "module-product-mk1" )
